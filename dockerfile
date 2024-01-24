c03683864cb1bd4c75a4279db703212645e9db52
miri price <mirip3541@gmail.com>
2024-01-24 23:08:38 +0200

    clean and fix the code

--- dockerfile
+++ dockerfile
@@ -1,15 +1,3 @@
-# # # Use the official Python image
-#  FROM python:3.8
-
-# # Set the working directory in the container
-# WORKDIR /app
-
-# # Copy the script into the container
-# COPY script.py /app
-
-# # Run the script with CMD allowing command-line arguments
-# CMD ["python", "script.py"]
-
 # Use the official Python image
 FROM python:3.8
 
--- script.py
+++ script.py
@@ -1,39 +1,3 @@
-# import os
-# def single_number(nums):
-#     xor_sum = 0
-#     for num in nums:
-#         xor_sum ^= num
-#     return xor_sum
-
-# a = [int(num) for num in os.environ["XOR_NUMBERS"].split()]
-# print(single_number(a))
-# print("hello world")
-# print("have a good day (: ")
-
-# # nums_input = input("Enter space-separated numbers: ")
-# # numbers = [int(num) for num in nums_input.split()]
-# # result = single_number(numbers)
-# # print(f"XOR sum: {result}")
-# # print("hello world")
-# # print("have a good day (: ")
-# import sys
-
-# def single_number(nums):
-#     xor_sum = 0
-#     for num in nums:
-#         xor_sum ^= num
-#     return xor_sum
-
-# # Check if command-line arguments are provided
-# if len(sys.argv) < 2:
-#     print("Usage: python script.py <num1> <num2> ...")
-#     sys.exit(1)
-
-# # Extract numbers from command-line arguments
-# a = [int(arg) for arg in sys.argv[1:]]
-# print(f"XOR sum: {single_number(a)}")
-
-
 import os
 import sys
 
